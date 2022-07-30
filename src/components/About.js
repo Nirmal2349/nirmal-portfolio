716c31db7982a66e74fbacc9025e40a2b4281ddc
Nirmal2349 <nirmalrajr35@gmail.com>
2022-07-30 15:11:47 +0530

    no style

--- src/components/About.js
+++ src/components/About.js
@@ -7,63 +7,69 @@ import { Cube } from "./Cube";
 import CV from "../assets/resume.pdf";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 
 export function About() {
   return (
-    <div className="about_data">
-      <div className="about_title">
-        <div>
-          <h1>
-            Hi, <br></br> I'm <span className="about_color">S Nirmal</span>
-            <br></br>Full-Stack Developer
-          </h1>
-          <Stack spacing={8} direction="row">
-            <a href={CV} download className="atag">
-              <Button variant="outlined">Download cv</Button>
-            </a>
-            <Button
-              aria-label="contact"
-              variant="outlined"
-              style={{ color: "lightgrey" }}
-            >
-              <NavLink className="link" to="/Contact">
-                Contact
-              </NavLink>
-            </Button>
-          </Stack>
-
-          <br></br>
-          <div className="about_social">
-            <a
-              href="https://github.com/Nirmal2349"
-              rel="noreferrer"
-              activeClassName="active"
-              target="_blank"
-            >
-              <GitHubIcon style={{ color: "Maximum blue green" }} />
-            </a>
-            <a
-              href="https://linkedin.com/in/nirmalraj2349"
-              rel="noreferrer"
-              activeClassName="active"
-              target="_blank"
-            >
-              <LinkedInIcon style={{ color: "Maximum blue green" }} />
-            </a>
-            <a
-              href="https://twitter.com/nirmalraj35"
-              rel="noreferrer"
-              activeClassName="active"
-              target="_blank"
-            >
-              <TwitterIcon style={{ color: "Maximum blue green" }} />
-            </a>
+    <Box>
+      <Grid container>
+        <Grid item xs={12} sm={4} md={6} sx={{ background: "lightblue" }}>
+          <div className="about_cubic">
+            <Cube />
           </div>
-        </div>
-      </div>
-      <div className="about_cubic">
-        <Cube />
-      </div>
-    </div>
+        </Grid>
+        <Grid item xs={12} sm={8} md={6} sx={{ background: "lightgreen" }}>
+          <div className="about_title">
+            <h1>
+              Hi, <br></br> I'm <span className="about_color">S Nirmal</span>
+              <br></br>Full-Stack Developer
+            </h1>
+            <Stack spacing={8} direction="row">
+              <a href={CV} download className="atag">
+                <Button variant="outlined">Download cv</Button>
+              </a>
+              <Button
+                aria-label="contact"
+                variant="outlined"
+                style={{ color: "lightgrey" }}
+              >
+                <NavLink className="link" to="/Contact">
+                  Contact
+                </NavLink>
+              </Button>
+            </Stack>
+
+            <br></br>
+            <div className="about_social">
+              <a
+                href="https://github.com/Nirmal2349"
+                rel="noreferrer"
+                activeClassName="active"
+                target="_blank"
+              >
+                <GitHubIcon style={{ color: "Maximum blue green" }} />
+              </a>
+              <a
+                href="https://linkedin.com/in/nirmalraj2349"
+                rel="noreferrer"
+                activeClassName="active"
+                target="_blank"
+              >
+                <LinkedInIcon style={{ color: "Maximum blue green" }} />
+              </a>
+              <a
+                href="https://twitter.com/nirmalraj35"
+                rel="noreferrer"
+                activeClassName="active"
+                target="_blank"
+              >
+                <TwitterIcon style={{ color: "Maximum blue green" }} />
+              </a>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,87 @@
 import React from "react";
 import { Exp } from "./experience";
-  
+import IMG1 from "../assets/movie.jpg";
+import IMG2 from "../assets/color.jpg";
+import IMG3 from "../assets/tictactoe.jpg";
+import IMG4 from "../assets/bday.jpg";
+import Button from "@mui/material/Button";
 
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Movie-App",
+    github: "https://github.com/Nirmal2349/react-app",
+    demo: "https://movie-app-tictactoe.netlify.app",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Color-Game",
+    github: "https://github.com/Nirmal2349/react-app",
+    demo: "https://movie-app-tictactoe.netlify.app",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Tic-Tac-Toe",
+    github: "https://github.com/Nirmal2349/react-app",
+    demo: "https://movie-app-tictactoe.netlify.app",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Birthday calculator",
+    github: "https://github.com/Nirmal2349/dom-birthday-calc-2",
+    demo: "https://loving-goldstine-3a6a1b.netlify.app/",
+  },
+];
 
 export function Project() {
   return (
-   <div>
-  project title
-  
-    <Exp/>
-   </div>
-    
+    <div>
+      <br></br>
+      <br></br>
+      <Exp />
 
-  )
+      <section id="portfolio">
+        <div className="project_head">
+          <h2>Portfolio</h2>
+        </div>
+        <div className="container portfolio__container">
+          {data.map((item) => (
+            <article className="portfolio__item" key={item.id}>
+              <div className="portfolio__item-img">
+                <img src={item.image} alt={`portfolio${item.id}`} />
+              </div>
+              <h3 style={{ textAlign: "center", color:"beige" }}>{item.title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={item.github}
+                  className="atag"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button variant="outlined">Github</Button>
+                </a>
+             
+                <a
+                  href={item.demo}
+                  className="atag"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Button variant="outlined">Live Demo</Button>
+                </a>
+              </div>
+            </article>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
   }
 
