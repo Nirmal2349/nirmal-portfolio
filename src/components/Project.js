5e9b63459f69d488da05c57d9376e3a36f421482
nirmal <nirmalrajr35@gmail.com>
2022-10-03 11:38:55 +0530

    new

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Exp } from "./experience";
+import IMG00 from "../assets/bags.JPG";
+import IMG01 from "../assets/login.JPG";
 import IMG1 from "../assets/movie.jpg";
 import IMG2 from "../assets/color.jpg";
 import IMG3 from "../assets/tictactoe.jpg";
@@ -12,27 +14,41 @@ import { Animationrouter } from "./animationrouter";
 const data = [
   {
     id: 1,
+    image: IMG00,
+    title: "Shopping-Bags",
+    github: "https://github.com/Nirmal2349/mern-bags",
+    demo: "https://mern-bag.herokuapp.com/",
+  },
+  {
+    id: 2,
+    image: IMG01,
+    title: "Login-App",
+    github: "https://github.com/Nirmal2349/login-authentication",
+    demo: "https://login-app.herokuapp.com/",
+  },
+  {
+    id: 3,
     image: IMG1,
     title: "Movie-App",
     github: "https://github.com/Nirmal2349/react-app",
     demo: "https://movie-app-tictactoe.netlify.app",
   },
   {
-    id: 2,
+    id: 4,
     image: IMG2,
     title: "Color-Game",
     github: "https://github.com/Nirmal2349/react-app",
     demo: "https://movie-app-tictactoe.netlify.app",
   },
   {
-    id: 3,
+    id: 5,
     image: IMG3,
     title: "Tic-Tac-Toe",
     github: "https://github.com/Nirmal2349/react-app",
     demo: "https://movie-app-tictactoe.netlify.app",
   },
   {
-    id: 4,
+    id: 6,
     image: IMG4,
     title: "Birthday calculator",
     github: "https://github.com/Nirmal2349/dom-birthday-calc-2",
